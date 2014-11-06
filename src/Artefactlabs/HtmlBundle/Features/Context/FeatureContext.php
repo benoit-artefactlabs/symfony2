6bcba5022be4b96dd4c266b36d6fd96e7f512f07
benoit.git <bencom@artefactlabs.net>
2014-11-06 19:01:27 +0100

    corrected, behat SF2 implementation

--- src/Artefactlabs/HtmlBundle/Features/Context/FeatureContext.php
+++ src/Artefactlabs/HtmlBundle/Features/Context/FeatureContext.php
@@ -2,51 +2,8 @@
 
 namespace Artefactlabs\HtmlBundle\Features\Context;
 
-/*
 use Symfony\Component\HttpKernel\KernelInterface;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
-use Behat\Symfony2Extension\Context\KernelAwareInterface;
-use Behat\Symfony2Extension\Context\KernelDictionary;
-use Behat\MinkExtension\Context\MinkContext;
-use Behat\Behat\Context\BehatContext;
-use Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
-
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
-
-class FeatureContext extends MinkContext //MinkContext if you want to test web, BehatContext
-                  implements KernelAwareContext
-{
-    private $kernel;
-    private $parameters;
-
-    public function __construct(array $parameters)
-    {
-        $this->parameters = $parameters;
-    }
-
-    public function setKernel(KernelInterface $kernel)
-    {
-        $this->kernel = $kernel;
-    }
-
-//
-// Place your definition and hook methods here:
-//
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        $container = $this->kernel->getContainer();
-//        $container->get('some_service')->doSomethingWith($argument);
-//    }
-//
-}
-*/
 use Behat\MinkExtension\Context\MinkContext;
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
@@ -55,13 +12,19 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
-/**
- * Features context.
- */
-class FeatureContext extends MinkContext
+class FeatureContext extends MinkContext implements KernelAwareContext
 {
-    public function __construct(/*array $parameters*/)
+
+    private $kernel;
+
+    public function __construct()
     {
         //$this->useContext('subcontext_alias', new AnotherContext());
     }
+
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
 }
